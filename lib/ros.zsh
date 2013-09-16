270266bc53faedcf82a47cba5598ab90ac206563
Eric Timmons <etimmons@mit.edu>
2013-09-16 15:06:56 -0400

    updating ROS setup file location.

--- lib/ros.zsh
+++ lib/ros.zsh
@@ -1,7 +1,7 @@
 # Set ROS_ZSH_SETUP_FILE_LOCATION if different from the default.
 
 if [[ -z "$ROS_ZSH_SETUP_FILE_LOCATION" ]]; then
-   ROS_ZSH_SETUP_FILE_LOCATION="$HOME/mers-workspace/ros/setup.zsh"
+   ROS_ZSH_SETUP_FILE_LOCATION="$HOME/mers-workspace/ros/hydro/setup.zsh"
 fi
 
 if [ -e $ROS_ZSH_SETUP_FILE_LOCATION ]
