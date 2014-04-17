74fc3600c3d037b5f5b5911051562ef9316f0998
Eric Timmons <etimmons@mit.edu>
2014-04-17 20:27:12 +1000

    Updating ROS location.

--- lib/ros.zsh
+++ lib/ros.zsh
@@ -2,7 +2,7 @@
 
 load_ros(){
     if [[ -z "$ROS_ZSH_SETUP_FILE_LOCATION" ]]; then
-	    ROS_ZSH_SETUP_FILE_LOCATION="$HOME/mers-workspace/ros/hydro/setup.zsh"
+	    ROS_ZSH_SETUP_FILE_LOCATION="$HOME/mers-workspace/ros/hydro/roslisp_overlay/dry/setup.zsh"
     fi
 
     if [ -e $ROS_ZSH_SETUP_FILE_LOCATION ]
